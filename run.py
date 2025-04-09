5c0e17a07d16e4639e2390c20afff50258ffb190
AlDas23 <alex.dascalu1@gmail.com>
2025-04-09 12:31:24 +0300

    Add streaming input and Qwen2.5 3B (for testing)

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import gradio as gr
 import webui.webuiApp as webui
 
 if __name__ == "__main__":
--- webui/webuiApp.py
+++ webui/webuiApp.py
@@ -1,24 +1,27 @@
 import gradio as gr
 from app.scripts import *
 
-def processRequest(image, prompt):
-    # TODO Add mechanism for getting n-th frame from video
-    frame = getFrame(image)
-    frameData = exctractFrameData(frame)
-    
-    response = generateResponse(prompt, frameData)
-    return response
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            input_img = gr.Image(
+                label="Input", sources="webcam", streaming=True, type="pil"
+            )
+            input_prompt = gr.Textbox(label="Prompt", show_label=True)
+        with gr.Column():
+            output_prompt = gr.Textbox(label="Result")
 
-demo = gr.Interface(
-    fn=processRequest,
-    inputs=[
-        gr.Image(sources=["webcam"], streaming=True, tool="select", type="pil"),
-        gr.Textbox(label="Prompt", show_label=True),
-    ],
-    outputs=[gr.Textbox(label="Result")],
-    title="AI Video Interpreter",
-    live=True,
-    flagging_mode="never",
-)
+        def processRequest(image, prompt):
+            frame = getFrame(image)
 
+            response = generateResponse(prompt, frame)
+            return response
 
+        input_img.stream(
+            fn=processRequest,
+            inputs=[input_img, input_prompt],
+            outputs=output_prompt,
+            time_limit=15,
+            stream_every=2,
+            concurrency_limit=None,
+        )
