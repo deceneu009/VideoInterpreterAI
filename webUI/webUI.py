8d52262ff9260d3a831ccec5be604ca76d0514e0
deceneu009 <sstefy8967@gmail.com>
2025-04-16 15:58:35 +0300

    Default start

---
+++ webUI/webUI.py
@@ -0,0 +1,21 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            input_img = gr.Image(label="Input", sources="webcam")
+            input_txt = gr.Textbox(label="Prompt")
+        with gr.Column():
+            output_img = gr.Textbox(label="Output")
+            input_img.stream(
+                lambda s: s,
+                input_img,
+                output_img,
+                time_limit=15,
+                stream_every=0.1,
+                concurrency_limit=30,
+            )
+
+
+if __name__ == "__main__":
+    demo.launch()
