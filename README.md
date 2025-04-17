001127316466f2811692f2633ec6595a55b34d33
deceneu009 <sstefy8967@gmail.com>
2025-04-17 16:58:16 +0300

    Basic start. Implement a webUI with gradio and generate a requirements.txt with the basic packages for the project. README update with TODO and steps

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # VideoInterpreterAI
-
 ## Stefan's branch
 
-Currect model used: Qwen2.5-VL-3B-Instruct
+### Done:
 
-Testing results: Fail. Reccomended to switch to NanoLLM and probably switch model.
+Interface: DONE
 
-Interface status: Works
+### TODO:
+
+##### For real time:
+- Implement a model
+- Make it take as input the video
+- Parse the input from the prompt
+- feed it to the AI
+- get the output and feed it to the webUI
+
+##### For the Analytic part:
+- Use the same model(optional, but preferred)
+- After pressing record, save the video stream in a directory
+- Make the model analyze the stream
+- Generate an output and feed it to the webUI
--- app/scripts.py
+++ app/scripts.py
@@ -1,20 +1,6 @@
 import cv2
 import numpy as np
 from PIL import Image
-from transformers import (
-    Qwen2_5_VLForConditionalGeneration,
-    AutoTokenizer,
-    AutoProcessor,
-)
-from qwen_vl_utils import process_vision_info
-
-# Initialize Qwen model and tokenizer
-model_name = "Qwen/Qwen2.5-VL-3B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2.5-VL-3B-Instruct", torch_dtype="auto", device_map="auto"
-)
 
 
 def getFrame(image):
@@ -50,35 +36,35 @@ def generateResponse(prompt, image):
         }
     ]
 
-    text = processor.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
+    # text = processor.apply_chat_template(
+    #    messages, tokenize=False, add_generation_prompt=True
+    # )
 
-    inputs = processor(
-        text=[text],
-        images=[image],
-        padding=True,
-        return_tensors="pt",
-    )
-    inputs = inputs.to("cuda")
+    # inputs = processor(
+    #    text=[text],
+    #    images=[image],
+    #    padding=True,
+    #    return_tensors="pt",
+    # )
+    # inputs = inputs.to("cuda")
 
-    # INFO: Relies on torchvision and torch+cuda, but current torch+cu version conflicts with torchvision
-    generated_ids = model.generate(
-        **inputs,
-        max_new_tokens=128,
-        do_sample=True,
-        temperature=0.7,
-        top_p=0.9,
-    )
+    ## INFO: Relies on torchvision and torch+cuda, but current torch+cu version conflicts with torchvision
+    #  generated_ids = model.generate(
+    #    **inputs,
+    #    max_new_tokens=128,
+    #    do_sample=True,
+    #    temperature=0.7,
+    #    top_p=0.9,
+    # )
 
-    # Decode the entire output
-    raw_output = processor.batch_decode(
-        generated_ids,
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=True,
-    )
+    ## Decode the entire output
+    # raw_output = processor.batch_decode(
+    #    generated_ids,
+    #    skip_special_tokens=True,
+    #    clean_up_tokenization_spaces=True,
+    # )
 
-    # Extract the actual response (usually after the prompt)
-    response = raw_output[0].split("Assistant:")[-1].strip()
-    
-    return response
+    ## Extract the actual response (usually after the prompt)
+    # response = raw_output[0].split("Assistant:")[-1].strip()
+    #
+    # return response
