ef42ce083abe80962e9e4c9c2aa78ffa1a94eaf2
AlDas23 <alex.dascalu1@gmail.com>
2025-05-14 12:09:59 +0300

    Update get_model and generateResponse

--- app/scripts.py
+++ app/scripts.py
@@ -7,27 +7,23 @@ model_cache = {}  # Cache for models
 def get_model(model):
     # TODO: Check if model loaded correctly
     if model not in model_cache:
-        chat_history = ChatHistory(
-            model, system_prompt="You are an assistant who perfectly describes images."
-        )
-        model_cache[model] = NanoLLM.from_pretrained(
-            model=model,
-            chat_history=chat_history,
-            max_context_len=2048,
-            max_new_tokens=512,
-        )
+        model_cache[model] = NanoLLM.from_pretrained(model=model)
     return model_cache[model]
 
 
 def generateResponse(prompt, image):
     # TODO: Tweak model response argumets
     model = get_model("Efficient-Large-Model/VILA-7b")
-    response = model.chat(
-        prompt,
-        image=image,
-        temperature=0.7,
-        top_p=0.9,
-        top_k=50,
-        num_beams=1,
+
+    inputs = {
+        "user": "user",
+        "content": [
+            {"type": "text", "text": prompt},
+            {"type": "image", "image": image},
+        ],
+    }
+    response = model.generate(
+        inputs=inputs,
+        streaming=False,
     )
     return response
