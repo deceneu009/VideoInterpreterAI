d55345057146be310a72d91116331a5058623943
deceneu009 <sstefy8967@gmail.com>
2025-05-27 22:47:10 +0300

    Fixed interface

--- webui/webuiApp.py
+++ webui/webuiApp.py
@@ -1,6 +1,13 @@
 import gradio as gr
 from app.scripts import *
 
+# Main processing function: runs whenever the webcam stream is active
+
+def processRequest(image, prompt):
+    frame = getFrame(image)
+    response = generateResponse(prompt, frame)
+    return response
+
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
@@ -11,17 +18,16 @@ with gr.Blocks() as demo:
         with gr.Column():
             output_prompt = gr.Textbox(label="Result")
 
-        def processRequest(image, prompt):
-            frame = getFrame(image)
+    # Stream frames continuously and process each as long as the user has activated the webcam
+    input_img.stream(
+        fn=processRequest,
+        inputs=[input_img, input_prompt],
+        outputs=[output_prompt],
+        time_limit=None,
+        stream_every=1,
+        concurrency_limit=None,
+    )
 
-            response = generateResponse(prompt, frame)
-            return response
+# Launch the demo application
+demo.launch()
 
-        input_img.stream(
-            fn=processRequest,
-            inputs=[input_img, input_prompt],
-            outputs=output_prompt,
-            time_limit=15,
-            stream_every=10,
-            concurrency_limit=None,
-        )
